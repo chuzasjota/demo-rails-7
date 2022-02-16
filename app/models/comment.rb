98418894ab843b4c19d73f382b52a561e8e661ef
chuzasjota <chuzas.j@gmail.com>
2022-02-16 16:27:22 -0500

    Add info from annotate gem

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :bigint           not null, primary key
+#  content    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  post_id    :bigint           not null
+#
+# Indexes
+#
+#  index_comments_on_post_id  (post_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (post_id => posts.id)
+#
 class Comment < ApplicationRecord
   belongs_to :post
   broadcasts_to :post
